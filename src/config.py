6f61da1f293f03b20b2e7233bf4d755d96f5c3a1
Daniel0109-dot <danieljoaquim0109@gmail.com>
2025-12-17 23:32:13 -0300

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,18 @@
+# src/config.py
+from datetime import datetime
+import os
+
+DATA_FMT = "%d-%m-%Y"
+DATA_HOJE = datetime.today().strftime(DATA_FMT)
+
+PASTA_PROCESSADO = "./input"
+PASTA_SAIDA = "./output"
+
+os.makedirs(PASTA_SAIDA, exist_ok=True)
+
+COLUNAS_ESSENCIAIS = [
+    "código da vaga", "rap", "diretoria", "vaga", "status da vaga", "cargo",
+    "estado", "cidade da vaga", "responsável", "email do recrutador(a)",
+    "data de criação", "data da última modificação", "recrutador", "status gupy",
+    "cidade", "data criação",
+]
